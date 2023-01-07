ac4330200620e202146e0c220c5eacae98f5d68b
Ayush Singh <ayush.vijay@gmail.com>
2023-01-07 17:26:13 +0000

    nvim - added rust-analyzer

--- nvim/.config/nvim/lua/singhayu/lsp.lua
+++ nvim/.config/nvim/lua/singhayu/lsp.lua
@@ -1,6 +1,7 @@
 -- lspconfig
 local nvim_lsp = require('lspconfig')
-local servers = { 'pyright', 'tsserver' }
+local servers = { 'pyright', 'tsserver', 'rust-analyzer' }
+
 
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
